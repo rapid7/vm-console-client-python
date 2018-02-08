317f4dad91dd716c85210261c1f820f0b5411fd6
Zachary Youtz <zachary_youtz@rapid7.com>
2018-02-07 22:26:40 -0500

    Add license, ignore files, and update setup_workspace

--- setup_workspace/setup_workspace.py
+++ setup_workspace/setup_workspace.py
@@ -1,18 +1,46 @@
+# setup_workspace.py
+#
+# Purpose: Generation of Python based API client for the Rapid7 Nexpose and InsightVM v3 API
+#
+# Requirements: Java install and on PATH
+#
+# Configuration: Update CONSOLE_URL parameter with appropriate value
+#
+# Output: Generation of api and models based on Rapid7 Nexpose and InsightVM Swagger file
+#
+
 import os
 import shutil
 import ssl
 import urllib.request
 
-# Download swagger codegen jar
-jar_name = "swagger-codegen-cli"
-jar_version = "2.3.0"
-url = "http://central.maven.org/maven2/io/swagger/%s/2.3.0/%s-%s.jar" % (jar_name, jar_name, jar_version)
+# Console connection details to pull swagger file
+console_url = "https://localhost:3780"
+console_user = "nxadmin"
+console_pass = "nxpassword"
 
-urllib.request.urlretrieve(url, "%s-%s.jar" % (jar_name, jar_version))
+gem_version = "0.0.1"
+
+# Download swagger codegen jar
+codegen_jar_name = "swagger-codegen-cli"
+codegen_jar_version = "2.3.0"
+url = "http://central.maven.org/maven2/io/swagger/%s/2.3.0/%s-%s.jar" % (codegen_jar_name, codegen_jar_name, codegen_jar_version)
+
+urllib.request.urlretrieve(url, "setup_workspace/%s-%s.jar" % (codegen_jar_name, codegen_jar_version))
+
+# Fetch console version
+#urllib.request.urlretrieve(console_url + "/api/3/administration/info")
+
+console_version = "6.5.5"
+
+# Manage API release dates
+api_file_dir = 'api-files/'
+tracking_file = 'api_history.json'
+previous_version = '0.0.0'
 
 # Download swagger file
-console_swagger_path = "https://localhost:3780/api/3/json"
-swagger_file = "json"
+console_swagger_path = console_url + "/api/3/json"
+swagger_file = api_file_dir + "console-swagger-%s.json" % console_version
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -22,11 +50,16 @@ with urllib.request.urlopen(console_swagger_path, context=ctx) as u, open(swagge
     f.write(u.read())
 
 # Remove previous directories from generation
-dirs = ['lib', 'docs', 'spec']
-
-for directory in dirs:
-    if os.path.exists(directory):
-        shutil.rmtree(directory)
+# dirs = ['lib', 'docs', 'spec']
+#
+# for directory in dirs:
+#     if os.path.exists(directory):
+#         shutil.rmtree(directory)
 
 # Generate library
-os.system("java -jar %s-%s.jar generate -i %s -l python -o ./ -c config.json" % (jar_name, jar_version, swagger_file))
+codegen_jar = "setup_workspace/%s-%s.jar" % (codegen_jar_name, codegen_jar_version)
+os.system(("java -jar %s generate -i %s -l python "
+           "--git-user-id \"rapid7\" "
+           "--git-repo-id \"vm-console-client-python\" "
+           "--release-note \"Update generated gem to version: %s\" "
+           "-o ./ -c setup_workspace/config.json") % (codegen_jar, swagger_file, gem_version))
