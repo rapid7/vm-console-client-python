c7bbc7c8964a931c8d3d83d5831300b01f7137e3
Rigen Mehilli <rmehilli@bos-mbp-5353.local>
2018-08-08 14:46:20 -0400

    Added bash script to generate library

---
+++ setup_workspace/setup_workspace.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Download InsightVM/Nexpose Console Version
+VERSION_URL="http://download2.rapid7.com/download/InsightVM/Rapid7Setup-Linux64.bin.version"
+CONSOLE_VERSION=$(curl $VERSION_URL)
+LIB_VERSION="0.0.1-$CONSOLE_VERSION"
+echo "Library Version: $LIB_VERSION"
+
+# Download swagger file
+API_FILE_DIR="api-files/"
+SWAGGER_FILE=$API_FILE_DIR"console-swagger.json"
+echo "SWAGGER FILE: $SWAGGER_FILE"
+SWAGGER_URL="https://help.rapid7.com/insightvm/en-us/api/api.json"
+SWAGGER=$(curl $SWAGGER_URL)
+echo "$SWAGGER" > $SWAGGER_FILE
+
+# Manage swagger codegen
+CODEGEN_JAR_NAME="swagger-codegen-cli"
+CODEGEN_JAR_VERSION="2.3.0"
+URL="http://central.maven.org/maven2/io/swagger/$CODEGEN_JAR_NAME/2.3.0/$CODEGEN_JAR_NAME-$CODEGEN_JAR_VERSION.jar"
+JAR_PATH="setup_workspace/$CODEGEN_JAR_NAME-$CODEGEN_JAR_VERSION.jar"
+echo "URL: $URL and PATH: $JAR_PATH"
+
+# Download and write to file
+JAR=$(curl $URL)
+echo "$JAR" > $JAR_PATH
+
+# Generate Library
+CODEGEN_JAR="setup_workspace/$CODEGEN_JAR_NAME-$CODEGEN_JAR_VERSION.jar"
+echo "CODEGEN_JAR: $CODEGEN_JAR"
+
+git status --porcelain --untracked-files=no
+
+# Check if changes were made - new console version | new swagger file
+if [[ `git status --porcelain --untracked-files=no` ]]; then
+  # Changes
+  COMMAND="java -jar $CODEGEN_JAR generate -i $SWAGGER_FILE -l python \
+           --git-user-id \"rmehilli-r7\" \
+           --git-repo-id \"vm-console-client-python\" \
+           --release-note \"Update generated library to version: $LIB_VERSION\" \
+           -o ./ -c setup_workspace/config.json"
+	echo "$COMMAND"
+else
+  # No changes
+  echo 'No changes were made to client'
+fi
